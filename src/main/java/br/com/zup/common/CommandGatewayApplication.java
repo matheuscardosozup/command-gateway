611b28a8575377e8f3f802f3f93ebeb70d8f5aa0
MATHEUS PEREIRA CARDOSO <matheus.cardoso@portoseguro.com.br>
2023-01-30 14:40:52 -0300

    initial commit

---
+++ src/main/java/br/com/zup/common/CommandGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandGatewayApplication.class, args);
+	}
+
+}
